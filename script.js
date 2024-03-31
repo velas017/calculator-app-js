bc2e3ef365b4b0f71c47ad17a8d232c82e3288ed
Edar Velasquez <velasquezedarsw@gmail.com>
2024-03-31 12:52:28 -0500

    adding javascript

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ const expressionDiv = document.getElementById('expression');
 const resultDiv = document.getElementById('result');
 
 // Define expression and result variable
+let expression = '';
+let result = '';
+
+//Define event handler for button clicks
+
+function buttonClick(event) {
+    // Get values from clicked button
+    const target = event.target;
+    const action = target.dataset.action;
+    const value = target.dataset.value;
+
+    //Switch case to control the calculator
+    switch (action) {
+        case 'number':
+            console.log('number');
+            break;
+        case 'clear':
+            console.log('clear');
+            break;
+    }
+}
+
+inputBox.addEventListener('click', buttonClick);
